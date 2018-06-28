0d6c9d8ea808d697b40b43ee202428f1b3fb73ed
Pablo Vicente Munuera <pvicente1@us.es>
2018-06-28 17:09:42 +0200

    working with bugs

--- DrawingFrames/src/createFrameFromImage.m
+++ DrawingFrames/src/createFrameFromImage.m
@@ -7,8 +7,10 @@ function [ ] = createFrameFromImage( imgFile, fileName )
     imgWS = watershed(1 - logicalImg);
     l_img = bwlabel(imgWS);
     
-    noValidCells = unique(vertcat(l_img(1, :), l_img(:, 1)', l_img(end, :), l_img(:, end)'));
-    validCells = setdiff(1:max(l_img(:)), noValidCells);
+
+    %noValidCells = unique(vertcat(l_img(1, :), l_img(:, 1)', l_img(end, :), l_img(:, end)'));
+    %validCells = setdiff(1:max(l_img(:)), noValidCells);
+    validCells = 1:max(l_img(:));
     
     [neighs_real, ~] = calculateNeighbours(l_img);
     [ verticesInfo ] = calculateVertices( l_img, neighs_real);
@@ -16,9 +18,9 @@ function [ ] = createFrameFromImage( imgFile, fileName )
     vertices = verticesInfo.verticesPerCell;
     neighbours_vertices = verticesInfo.verticesConnectCells;
     
-    [~, goodIds] = unique(vertcat(vertices{:}), 'rows');
-    vertices = vertices (goodIds);
-    neighbours_vertices = neighbours_vertices(goodIds, :);
+%     [~, goodIds] = unique(vertcat(vertices{:}), 'rows');
+%     vertices = vertices (goodIds);
+%     neighbours_vertices = neighbours_vertices(goodIds, :);
     
     newOrderCells = [];
     for numVertex = 1:length(vertices)
@@ -28,14 +30,14 @@ function [ ] = createFrameFromImage( imgFile, fileName )
         end
     end
     
-    fileID = fopen(strcat('results/', fileName, '.frm'),'w');
+    fileID = fopen(strcat('D:/Pablo/Simulations/VertexModel/VertexModel/VertexModel/InitialFrames/', fileName, '.frm'),'w');
     %NumVertices
     fprintf(fileID, '%i\n', length(vertices));
     
     %Vertices: Index coorX coorY
     for numVertex = 1:size(vertices, 1)
-        coordinates = vertices{numVertex};
-        fprintf(fileID, '%i %d %d\n', numVertex-1, coordinates(1), coordinates(2));
+        coordinates = vertices{numVertex}/20;
+        fprintf(fileID, '%i %.3f %.3f\n', numVertex-1, coordinates(1), coordinates(2));
     end
     
     %NumCells
@@ -45,27 +47,29 @@ function [ ] = createFrameFromImage( imgFile, fileName )
     
     %Cells: NumVertices NumCell Growing Dying CellLine LastDivTime TimeInG0
     %GrowthStartTime IndicesVertices CellStatus
-    for numCell = newOrderCells
+    for numCell = 1:max(newOrderCells)
+        if any(newOrderCells == numCell) == 0
+            continue
+        end
         %verticesNumCell = arrayfun(@(x) any(numCell, x), neighbours_vertices);
         verticesNumCell = any(neighbours_vertices == numCell, 2);
         verticesActualCell = find(verticesNumCell);
         
-        K = convhull(vertcat(vertices{verticesNumCell}));
-        
+%         K = convhull(vertcat(vertices{verticesNumCell}));
+%         
 %         verticesToPaint = vertcat(vertices{verticesActualCell});
 %         plot(verticesToPaint(K, 1),verticesToPaint(K, 2),'r-',verticesToPaint(:, 1), verticesToPaint(:, 2),'b*')
-         
-        verticesActualCell = verticesActualCell(K(1:end-1));
+%         hold on;
+%         verticesActualCell = verticesActualCell(K(1:end-1));
         
         %NumVertices NumCell Growing Dying CellLine
-        fprintf(fileID, '%i %i 0 0 0 ', length(verticesActualCell), numCell);
+        fprintf(fileID, '%i %i 0 0 0 ', length(verticesActualCell), numCell-1);
         %LastDivTime TimeInG0 GrowthStartTime 
-        cl = clock;
-        fprintf(fileID, '%i %g 108000 ', toc(tStart)*500, cl(end-1) + cl(end));
+        fprintf(fileID, '0 0 0 ');
         %IndicesVertices
         fprintf(fileID, '%i ', verticesActualCell-1);
         %CellStatus
-        fprintf(fileID, '%g\n', toc(tStart));
+        fprintf(fileID, '1.001\n');
     end
     fprintf(fileID, '%i\n', numCell+1);
     fclose(fileID);
